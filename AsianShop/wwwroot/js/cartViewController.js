1ec295e4da80368a238fce56da876a8feec70dcc
Robert <rtvanh17@uia.com>
2020-08-24 23:06:37 +0200

    Added a new storing method, should change the navbar so when item is added it updates automatically

--- AsianShop/wwwroot/js/cartViewController.js
+++ AsianShop/wwwroot/js/cartViewController.js
@@ -1,35 +1,73 @@
 $(document).ready(function() {
-    let clientToServer = new ClientToServerController();
+    var items = localStorage.getItem("items");
+    var itemsObj = JSON.parse(items);
+    console.log(itemsObj);
+    
+    
     var cartView = new Vue({
         el: '#cartView',
         data: {
             totalPrice:0,
-            orderLines:[],
-            orderLine:{
-                id:0,
-                product:[],
-                productId:0,
+            items:[],
+            item:{
+                id: 0,
+                name: "",
+                price:0,
                 amount:0
+            }
             },
-        },
-        created: function () {
-            clientToServer.getOrderLine(this);
-        },
-        mounted(){
+            created:function(){
+                this.items = itemsObj;
 
-        $("#cartButton").click(function(){
-            clientToServer.getOrderLine(this);
-            let name = "cartView";
-            var x = document.getElementById(name);
-            if (x.style.display === "none") {
-                x.style.display = "block";
+                for(i = 0;i < itemsObj.length; i++){
+                    var price =  (itemsObj[i].price).replace(",",".");
+                    var amount = parseFloat(itemsObj[i].amount);
+                    var floatPrice = parseFloat(price);
+                    this.totalPrice += floatPrice*amount;
+                }
+            },
+            methods:{
+               deleteItem(index){
+                    this.items.splice(index,1);
+                    localStorage.setItem("items",JSON.stringify(this.items))
+               }
             }
-            //If setting is visible
-            else {
-                x.style.display = "none";
+        
+        
+    });
+    
+/*
+    $("#navbarDropdownMenuLink").click(function(params) {
+        if(itemsObj != null){
+            for(i = 0;i < itemsObj.length; i++){
+                var name = itemsObj[i].name;
+                var price = itemsObj[i].price;
+                var amount = itemsObj[i].amount;
+                
+                tr.appendChild(td);
+                td.append(name);
+                td.append(price);
+                td.append(amount);
+                td.append(delButton);
             }
-        });
+            tbody.appendChild(tr);
+        }*/
+            /*
+        var k = '<tbody>'
+          
+            for(i = 0;i < itemsObj.length; i++){
+                k+= '<tr>';
+                k+= '<td>' + itemsObj[i].name + '</td>';
+                k+= '<td>' + itemsObj[i].price + " kr"+ '</td>';
+                k+= '<td>' + itemsObj[i].amount + '</td>';
+                k+= '<td>' + delButton + '</td>';
+                k+= '</tr>';
+            }
+        
+            k+='</tbody>';
+            document.getElementById('tbody').innerHTML = k;
         }
     });
     
+          */
 });
--- AsianShop/wwwroot/js/clientViewController.js
+++ AsianShop/wwwroot/js/clientViewController.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    let itemList =[];
     let clientToServer = new ClientToServerController();
     var clientView = new Vue({
         el: '#clientView',
@@ -10,15 +11,67 @@ $(document).ready(function() {
                 productId:0,
                 amount:0
             },
+            products: [],
+            product: {
+                id: 0,
+                name: "",
+                price: 0,
+                file:"",
+                filePath: "",
+                amount:0,
+                from:"",
+                about:"",
+                unit:"",
+                type: "",
+                typeId:0
+            },
         },
         methods: {
             addOrderLine: function(){
+                if(typeof(Storage) !== "undefined"){
+                    var item = {
+                        id : document.getElementById("addCart").value,
+                        name : document.getElementById("name").value,
+                        price : document.getElementById("price").value,
+                        amount : this.orderLine.amount
+                    }
+
+                    var oldItems = JSON.parse(localStorage.getItem("items"));
+                    if(oldItems == null){
+                        itemList.push(item);
+                    }
+                    else{
+                        for (const obj in oldItems) {
+                            //console.log(oldItems[obj]);
+                            itemList.push(oldItems[obj]);
+                        }
+
+                        //itemList.push(oldItems);
+                        itemList.push(item);
+                    }
+                    localStorage.setItem("items", JSON.stringify(itemList));
+                }
+                
+                function flatten(arr) {
+                    const result = []
+                  
+                    arr.forEach((i) => {
+                      if (Array.isArray(i)) {
+                        result.push(...flatten(i))
+                      } else {
+                        result.push(i)
+                      }
+                    })
+                    
+                    return result
+                  }
+                /*
                 var productId = document.getElementById("addCart").value;
                 this.orderLine.productId = productId;
                 let formData = new FormData();
                 formData.append('productId', this.orderLine.productId);
                 formData.append('amount', this.orderLine.amount);
-                clientToServer.postOrderLine(formData, this);
+                clientToServer.postOrderLine(formData, this);*/
             }
         },
         checkProductAmount:function () {
